7d4ff792f19b964b97a9829a38a1948cb565a058
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-16 13:39:24 +0100

    update build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import TrainerCard from './components/TrainerCard';
-import PokemonCards from './components/PokemonCards';
 import { Home } from "./pages";
 
 function App() {
