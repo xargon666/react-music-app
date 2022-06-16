5be3b5b03c55cd8c345df5ad63df5b29f6561b96
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-16 20:48:29 +0100

    add grid format to Main comp

--- src/layout/Main/index.js
+++ src/layout/Main/index.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import {Artist, Form} from '../../components'
+// React Imports
+import React from "react";
+import { Artist, Form } from "../../components";
+
+// Bootstrap Imports
+import { default as Container } from "react-bootstrap/Container";
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 const Main = () => {
   return (
     <>
-        <Artist/>
-        <Form/>
+      <Container>
+        <Row>
+          <Col lg={9}>
+            <Artist />
+          </Col>
+          <Col lg={3}>
+            <Form />
+          </Col>
+        </Row>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
